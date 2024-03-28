a92067c68a56f1d97ae89649b45eff22c8e12ee4
cl <1007746405@qq.com>
2024-03-28 16:47:17 +0800

    安全日志的列表查看页面，未完成：时间和颜色的绑定。部分url好像还用的是模拟的接口

--- src/views/listSL.vue
+++ src/views/listSL.vue
@@ -18,31 +18,12 @@ var lastPage = ref(1)
 var page=ref(1)
 
 const Info =ref([])
-// axios.get('/branch/getSafeDailyList?page=1&size=5').then(res => {
-//   Info.value= res.data.data
-//   console.log(res.data);
-//   console.log('!!!')
-//   console.log(Info.value);
-
-// })
 
 const valuea = ref(belong)
 // const optiona = [
 //   {
 //     value: 'fen1',
 //     label: '分公司1',
-//   },
-//   {
-//     value: 'fen2',
-//     label: '分公司2',
-//   },
-//   {
-//     value: 'fen3',
-//     label: '分公司3',
-//   },
-//   {
-//     value: 'fen4',
-//     label: '分公司4',
 //   },
 // ]
 // var optiona = []
@@ -52,60 +33,14 @@ var optionList =ref([{
   label:'所有分公司'
 }])
 var data = ref([])
-var tableData = ref([])
-
-
-
-
-const tableData2 = [
-  {
-    date: '2099-03-03',
-    name: 'xxx',
-    qian: '',
-    bian: '',
-    you: '',
-    dian: '',
-    huan: '',
-    cha:'xxx',
-  },
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    qian: 'California',
-    bian: 'Los Angeles',
-    you: 'No. ',
-    dian: 'CA 90036',
-    huan: 'CA 90036',
-  },
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    qian: 'California',
-    bian: 'Los Angeles',
-    you: 'No. ',
-    dian: 'CA 90036',
-    huan: 'CA 90036',
-  },
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    qian: 'California',
-    bian: 'Los Angeles',
-    you: 'No. ',
-    dian: 'CA 90036',
-    huan: 'CA 90036',
-  },
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    qian: 'California',
-    bian: 'Los Angeles',
-    you: 'No. ',
-    dian: 'CA 90036',
-    huan: 'CA 90036',
-  },
-
-]
+// var tableData = ref(
+//   {
+//     contentChecking:[],
+//     pid:[],
+//     itemDescribe:[]
+//   }
+// )
+var tableData=ref([])
 function pList(){
   axios.get('/petrol/getPetrolSafeDailyList?page='+page.value+'&size=10').then(res=>{
     tableData.value = res.data.data.data
@@ -117,7 +52,7 @@ function pList(){
   })
 }
 function cList1(){
-  axios.get('http://127.0.0.1:4523/m1/3942191-0-default/central/companyList').then(res=>{
+  axios.get('/central/companyList').then(res=>{
     let result=res.data.data
     // var optionList =[]
     console.log(result[0].companyId)
@@ -134,11 +69,19 @@ function cList1(){
   })
 }
 function cList2(){
-  axios.get('http://127.0.0.1:4523/m1/3942191-0-default/branch/getSafeDailyList?companyId='+belong+'&date='+dateValue.value).then(res=>{//分公司的companyId为其分公司编号，是固定的
+  axios.get('/branch/getSafeDailyList?companyId='+belong+'&date=2024-03-24').then(res=>{//分公司的companyId为其分公司编号，是固定的
+    // res.data.data.forEach(el=>{
+    //   tableData.value.contentChecking.push(el.contentChecking)
+    //   tableData.value.pid.push(el.pid)
+    //   tableData.value.itemDescribe.push(el.itemDescribe)
+    // })
+
     tableData.value = res.data.data
-    total.value=res.data.data.total
+    // total.value=res.data.data.total
     console.log(res.data.data)
     console.log(belong)
+    console.log(tableData.value)
+    console.log(tableData.value[0].contentChecking)
     console.log(dateValue.value)
   })
 }
@@ -146,7 +89,7 @@ onMounted(()=>{
   if(role==1){
     isCentral.value=true
     isCompany.value=true
-  cList1()
+    cList1()
 
   }else if(role==2){
     isBranch.value=true
@@ -158,11 +101,29 @@ onMounted(()=>{
     pList()
   }
 
+  // const formatter1 = (row, column, cellValue) => {
+  //   //return row.illegalType=='1'?'是':'否'
+  //   let cNum1=0
+  //   for(let i=1;i<9;i++){
+  //     cNum1+=row.contentChecking[i]
+  //   }
+  //   if(cNum1>0){color='white'}else {console.log(cNum1);console.log("gggg")}
+  //   // if((row.contentChecking[1]+row.contentChecking[2])=='1'){ return '打手机',console.log(row.contentChecking[1]+row.contentChecking[2]+row.contentChecking[3]),console.log(row.prop)}else if(row.illegalType=='2'){return '未正确摆放护栏'}else if(row.illegalType=='3'){
+  //   //   return '登高未戴安全帽'
+  //   // }else if(row.illegalType=='4'){
+  //   //   return '火灾'
+  //   // }else{
+  //   //     color='red'
+  //   //   // return '卸油区有人'
+  //   //  // return  row.itemDescribe.store
+  //   //  //  console.log(tableData.value[0].contentChecking[1])
+  //   //
+  //   // }
+  //
+  // }
+
 })
 
-// setup() {
-//   optiona();
-// }
 const size = ref<'default' | 'large' | 'small'>('default')
 
 const value1 = ref('')
@@ -195,9 +156,6 @@ function handleClick() {
 //
 //
 // }
-// function changeDate() {
-//   console.log(dateValue.value)
-// }
 function handlePageChange(currentPage)
 {
   page=currentPage
@@ -221,6 +179,39 @@ function handleDateChange(dateChoice){
     cList2()
   }
 }
+
+let color
+
+const formatter1 = (row, column, cellValue) => {
+  //return row.illegalType=='1'?'是':'否'
+  let cNum1=row.contentChecking[1]
+  for(let i=2;i<9;i++){
+    cNum1+=row.contentChecking[i]
+  }
+  if(cNum1>'1'){
+    color='red'
+    // return "red",
+    console.log(cNum1),
+        console.log(row.contentChecking[1])
+  }else {
+    color='green'
+    // return "green",
+    console.log("gggg")
+  }
+  // if((row.contentChecking[1]+row.contentChecking[2])=='1'){ return '打手机',console.log(row.contentChecking[1]+row.contentChecking[2]+row.contentChecking[3]),console.log(row.prop)}else if(row.illegalType=='2'){return '未正确摆放护栏'}else if(row.illegalType=='3'){
+  //   return '登高未戴安全帽'
+  // }else if(row.illegalType=='4'){
+  //   return '火灾'
+  // }else{
+  //     color='red'
+  //   // return '卸油区有人'
+  //  // return  row.itemDescribe.store
+  //  //  console.log(tableData.value[0].contentChecking[1])
+  //
+  // }
+
+}
+// const color1: {color:'purple'}
 
 
 
@@ -274,24 +265,44 @@ function handleDateChange(dateChoice){
     <div class="tmainBody">
       <el-table :data="tableData" style="width: 100%">
         <el-table-column prop="pid" label="加油站编号" align="center" width="245" />
-<!--        <el-table-column prop="{{dateValue.value}}" label="时间" width="120"  />-->
+        <!--        <el-table-column prop="{{dateValue.value}}" label="时间" width="120"  />-->
         <el-table-column label="巡检项目" align="center">
-          <el-table-column prop="qian" label="前庭" width="130" align="center" :formatter="formatter" />
-          <el-table-column prop="bian" label="便利店" width="130" align="center" :formatter="formatter" />
-          <el-table-column prop="you" label="油罐区" width="130" align="center" :formatter="formatter" />
-          <el-table-column prop="dian" label="电器设备" width="130" align="center" :formatter="formatter" />
-          <el-table-column prop="huan" label="环保" width="130" align="center" :formatter="formatter" />
+          <el-table-column prop="contentChecking.1" label="前庭"  width="130" align="center" :formatter="formatter1"  >
+            <svg viewBox="0 0 80 80" width="15" height="15">
+              <circle class="circle" cx="40" cy="40" r="38"/>
+            </svg>
+          </el-table-column>
+          <el-table-column prop="bian" label="便利店" width="130" align="center" :formatter="formatter" >
+            <svg viewBox="0 0 80 80" width="15" height="15">
+              <circle class="circle" cx="40" cy="40" r="38"/>
+            </svg>
+          </el-table-column>
+          <el-table-column prop="you" label="油罐区" width="130" align="center" :formatter="formatter" >
+            <svg viewBox="0 0 80 80" width="15" height="15">
+              <circle class="circle" cx="40" cy="40" r="38"/>
+            </svg>
+          </el-table-column>
+          <el-table-column prop="dian" label="电器设备" width="130" align="center" :formatter="formatter" >
+            <svg viewBox="0 0 80 80" width="15" height="15">
+              <circle class="circle" cx="40" cy="40" r="38"/>
+            </svg>
+          </el-table-column>
+          <el-table-column prop="huan" label="环保" width="130" align="center" :formatter="formatter" >
+            <svg viewBox="0 0 80 80" width="15" height="15">
+              <circle class="circle" cx="40" cy="40" r="38"/>
+            </svg>
+          </el-table-column>
         </el-table-column>
 
-          <el-table-column prop="links" label="" align="center" width="120px">
-            <template #header>
-              <!--              <el-input v-model="search" size="small" placeholder="Type to search" />-->
-            </template>
-            <template #default="scope">
+        <el-table-column prop="links" label="" align="center" width="120px">
+          <template #header>
+            <!--              <el-input v-model="search" size="small" placeholder="Type to search" />-->
+          </template>
+          <template #default="scope">
 
-              <!--              ！！链接-->
-              <el-link  @click="handleClick(scope.$index, scope.row)">查看详情</el-link>
-            </template>
+            <!--              ！！链接-->
+            <el-link  @click="handleClick(scope.$index, scope.row)">查看详情</el-link>
+          </template>
 
 
 
@@ -318,7 +329,7 @@ function handleDateChange(dateChoice){
 
   <div class="bodyPagelSL" v-else>
     <h1 id="title">安全日志查看</h1>
-<!--    <div style="font-size: 18px; font-weight: 700; text-align: center;padding-top: 40px;padding-left: 30px; margin-bottom: 10px">安全日志查看</div>-->
+    <!--    <div style="font-size: 18px; font-weight: 700; text-align: center;padding-top: 40px;padding-left: 30px; margin-bottom: 10px">安全日志查看</div>-->
     <div class="tmainBody">
       <el-table :data="tableData" style="width: 100%; margin: auto;align-items: center" :header-cell-style="{fontSize: '14px', backgroundColor: '#fff',border: 'none !important'}">
         <el-table-column prop="date" label="时间" align="center" width="220px" />
@@ -440,6 +451,8 @@ function handleDateChange(dateChoice){
   border-right: 1px solid #EBEEF5;
 }
 
-
+.circle {
+  fill: v-bind("color");
+}
 
 </style>
